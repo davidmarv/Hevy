50c934fefdac3744cb40b2a65ad0e6c9e5e606af
David M <dmarvin2020@gmail.com>
2025-01-28 15:18:08 -0600

    adding stringify to log full data

--- fetchWorkouts.js
+++ fetchWorkouts.js
@@ -1,10 +1,8 @@
-//test file to make sure api key is working
-
-require('dotenv').config(); 
+require('dotenv').config();
 const axios = require('axios');
 
 const fetchWorkouts = async () => {
-    const apiKey = process.env.HEVY_API_KEY; 
+    const apiKey = process.env.HEVY_API_KEY;
 
     if (!apiKey) {
         console.error('API Key is missing');
@@ -12,15 +10,18 @@ const fetchWorkouts = async () => {
     }
 
     try {
-        //pre made endpoint
+        // Pre-made endpoint
         const response = await axios.get('https://api.hevyapp.com/v1/workouts?page=1&pageSize=5', {
             headers: {
                 'accept': 'application/json',
-                'api-key': apiKey, 
+                'api-key': apiKey,
             },
         });
 
-        console.log('Workouts:', response.data);
+
+        //added this way to log the full data 
+        console.log('Full Workouts Data:', JSON.stringify(response.data, null, 2));
+
     } catch (error) {
         console.error('Error fetching workouts:', error.response?.data || error.message);
     }
